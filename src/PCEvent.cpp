bee23f9baa69f06a0fe4c9b0e5407d6b311472e2
k-natori <k_natori@mac.com>
2024-08-31 10:26:31 +0900

    First commit

---
+++ src/PCEvent.cpp
@@ -0,0 +1,491 @@
+
+#include <HTTPClient.h>
+#include <WiFiClient.h>
+
+#include "PCEvent.h"
+#include "NJScanner.h"
+
+float PCEvent::defaultTimezone = 0.0f;
+int PCEvent::currentYear = 0;
+int PCEvent::currentMonth = 0;
+int PCEvent::currentDay = 0;
+int PCEvent::nextMonthYear = 0;
+int PCEvent::nextMonth = 0;
+
+boolean PCEvent::_isCacheValid = false;
+
+String PCEvent::_rootCA;
+std::multimap<int, PCEvent> PCEvent::_eventsInThisMonth;
+std::multimap<int, PCEvent> PCEvent::_holidaysInThisMonth;
+std::vector<PCEvent> PCEvent::_eventsInNextMonth;
+
+PCEvent::PCEvent(String sourceString, float toTimezone)
+{
+    NJScanner scanner = NJScanner(sourceString);
+    do
+    {
+        String key = scanner.scanUpToString(":", true);
+        key.trim();
+        String content = scanner.scanUpToString("\r\n", true);
+        content.trim();
+
+        if (key == "DTSTART;VALUE=DATE")
+        {
+            _startTM = tmFromICalDateString(content, 0.0f);
+            _isDayEvent = true;
+        }
+        else if (key == "DTSTART")
+        {
+            _startTM = tmFromICalDateString(content, toTimezone);
+            _isDayEvent = false;
+        }
+        else if (key.startsWith("DTEND"))
+        {
+            _endTM = tmFromICalDateString(content, toTimezone);
+        }
+        else if (key == "SUMMARY")
+        {
+            _title = content;
+        }
+    } while (!scanner.isAtEnd());
+}
+PCEvent::PCEvent(int year, int month, int day, String title)
+{
+    tm timeInfo = {.tm_sec = 0, .tm_min = 0, .tm_hour = 0, .tm_mday = 0, .tm_mon = 0, .tm_year = 0};
+    timeInfo.tm_year = year - 1900;
+    timeInfo.tm_mon = month - 1;
+    timeInfo.tm_mday = day;
+    timeInfo.tm_wday = dayOfWeek(timeInfo.tm_year + 1900, timeInfo.tm_mon + 1, timeInfo.tm_mday);
+    _startTM = timeInfo;
+    _title = title;
+    _isDayEvent = true;
+}
+
+time_t PCEvent::getTimeT() const
+{
+    tm temp = _startTM;
+    return mktime(&temp);
+}
+int PCEvent::getYear()
+{
+    return _startTM.tm_year + 1900;
+}
+int PCEvent::getMonth()
+{
+    return _startTM.tm_mon + 1;
+}
+int PCEvent::getDay()
+{
+    return _startTM.tm_mday;
+}
+int PCEvent::getDayOfWeek()
+{
+    return _startTM.tm_wday;
+}
+int PCEvent::getHour()
+{
+    return _startTM.tm_hour;
+}
+int PCEvent::getMinute()
+{
+    return _startTM.tm_min;
+}
+int PCEvent::getSecond()
+{
+    return _startTM.tm_sec;
+}
+
+String PCEvent::descriptionForDay(boolean isToday)
+{
+    if (isToday)
+    {
+        if (_startTM.tm_hour > 0)
+        {
+            char buf[6];
+            sprintf(buf, "%02d:%02d", _startTM.tm_hour, _startTM.tm_min);
+            return String(buf);
+        }
+        else
+        {
+            return "Today:";
+        }
+    }
+    char buf[6];
+    sprintf(buf, "%d/%d", _startTM.tm_mon + 1, _startTM.tm_mday);
+    return String(buf);
+}
+
+double PCEvent::duration()
+{
+    return difftime(mktime(&_endTM), mktime(&_startTM));
+}
+String PCEvent::getTitle()
+{
+    return _title;
+}
+
+// static member functions
+void PCEvent::initialize(String rootCA, tm timeInfo, String holidayCacheString)
+{
+    PCEvent::setRootCA(rootCA);
+    PCEvent::setTimeInfo(timeInfo);
+    PCEvent::setHolidayCacheString(holidayCacheString);
+}
+
+void PCEvent::setRootCA(String newRootCA)
+{
+    PCEvent::_rootCA = newRootCA;
+}
+void PCEvent::setTimeInfo(tm timeInfo)
+{
+    PCEvent::currentYear = timeInfo.tm_year + 1900;
+    PCEvent::currentMonth = timeInfo.tm_mon + 1;
+    PCEvent::currentDay = timeInfo.tm_mday;
+    if (currentMonth == 12)
+    {
+        nextMonthYear = PCEvent::currentYear + 1;
+        nextMonth = 1;
+    }
+    else
+    {
+        nextMonthYear = PCEvent::currentYear;
+        nextMonth = currentMonth + 1;
+    }
+}
+void PCEvent::setHolidayCacheString(String cacheString)
+{
+    _isCacheValid = false;
+    if (cacheString.isEmpty())
+    {
+        return;
+    }
+    char monthString[7];
+    sprintf(monthString, "%04d%02d", PCEvent::currentYear, PCEvent::currentMonth);
+    if (cacheString.startsWith(monthString))
+    { // Holiday cache is valid
+        NJScanner scanner = NJScanner(cacheString);
+        scanner.scanUpToString("\n", true);
+        while (!scanner.isAtEnd())
+        {
+            String dayString = scanner.scanUpToString(":", false);
+            scanner.scanString(":");
+            String title = scanner.scanUpToString("\n", false);
+            if (!dayString.isEmpty())
+            {
+                int day = dayString.toInt();
+                PCEvent event = PCEvent(currentYear, currentMonth, day, title);
+                event.isHolidayEvent = true;
+                PCEvent::_holidaysInThisMonth.insert(std::make_pair(day, event));
+            }
+        }
+        _isCacheValid = true;
+    }
+}
+
+String PCEvent::holidayCacheString()
+{
+    String result = "";
+    char monthString[7];
+    sprintf(monthString, "%04d%02d", PCEvent::currentYear, PCEvent::currentMonth);
+    result += monthString;
+    result += "\n";
+    for (auto it = _holidaysInThisMonth.begin(); it != _holidaysInThisMonth.end(); ++it)
+    {
+        const auto& pair = *it;
+        int day = pair.first;
+        PCEvent event = pair.second;
+        result += day;
+        result += ":";
+        result += event.getTitle();
+        result += "\n";
+    }
+    return result;
+}
+boolean PCEvent::isCacheValid()
+{
+    return _isCacheValid;
+}
+
+boolean PCEvent::loadICalendar(String urlString, boolean holiday)
+{
+    HTTPClient httpClient;
+    httpClient.begin(urlString, PCEvent::_rootCA.c_str());
+    // dateString = "";
+
+    const char *headerKeys[] = {"Transfer-Encoding"};
+    httpClient.collectHeaders(headerKeys, 1);
+
+    int result = httpClient.GET();
+    if (result == HTTP_CODE_OK)
+    {
+
+        boolean chunked = (httpClient.header("Transfer-Encoding") == "chunked");
+        long chunkSize = 0;
+        boolean isChunkSizeLine = false;
+        boolean isTrailingLine = false;
+        String lastLine = "";
+        // dateString = httpClient.header("Date");
+        WiFiClient *stream = httpClient.getStreamPtr();
+        if (httpClient.connected())
+        {
+            String eventBlock = "";
+            boolean loadingEvent = false;
+
+            if (stream->available() && chunked)
+            {
+                chunkSize = intFrom16BaseString(stream->readStringUntil('\n'));
+            }
+
+            while (stream->available())
+            {
+                String line = stream->readStringUntil('\n');
+                if (chunked)
+                {
+                    if (isChunkSizeLine)
+                    {
+                        chunkSize = intFrom16BaseString(line);
+                        Serial.printf("new chunk: %ld\n", chunkSize);
+                        isChunkSizeLine = false;
+                        isTrailingLine = true;
+                        continue;
+                    }
+                    else if (isTrailingLine)
+                    {
+                        if (lastLine.length() > 1)
+                            lastLine = lastLine.substring(0, lastLine.length() - 1);
+                        chunkSize += lastLine.length();
+
+                        line = lastLine + line;
+
+                        isTrailingLine = false;
+                    }
+                    chunkSize -= (line.length() + 1);
+                    if (chunkSize <= 0)
+                    {
+                        lastLine = line;
+                        isChunkSizeLine = true;
+                        continue;
+                    }
+                }
+
+                if (line.startsWith("BEGIN:VEVENT"))
+                { // begin VEVENT block
+                    loadingEvent = true;
+                }
+                else if (line.startsWith("DTSTART"))
+                { // read start date
+                    int position = line.indexOf(":");
+                    String timeString = line.substring(position + 1);
+                    timeString.trim();
+                    tm timeInfo = tmFromICalDateString(timeString, PCEvent::defaultTimezone);
+                    if (!(timeInfo.tm_year + 1900 == PCEvent::currentYear && timeInfo.tm_mon + 1 == PCEvent::currentMonth) && !(timeInfo.tm_year + 1900 == nextMonthYear && timeInfo.tm_mon + 1 == nextMonth))
+                    {
+                        // discard event if not scheduled in this month to next month
+                        loadingEvent = false;
+                        eventBlock = "";
+                    }
+                }
+
+                if (loadingEvent)
+                {
+                    eventBlock += line + "\n";
+                }
+                if (loadingEvent && line.startsWith("END:VEVENT"))
+                {
+                    loadingEvent = false;
+                    PCEvent event = PCEvent(eventBlock, PCEvent::defaultTimezone);
+                    event.isHolidayEvent = holiday;
+                    if (event.getMonth() == PCEvent::currentMonth)
+                    {
+                        // Will be displayed as this month
+                        if (holiday)
+                        {
+                            PCEvent::_holidaysInThisMonth.insert(std::make_pair(event.getDay(), event));
+                        }
+                        else
+                        {
+                            PCEvent::_eventsInThisMonth.insert(std::make_pair(event.getDay(), event));
+                        }
+                    }
+                    else
+                    {
+                        // Next month
+                        _eventsInNextMonth.push_back(event);
+                    }
+                    eventBlock = "";
+                }
+            }
+        }
+        httpClient.end();
+        return true;
+    }
+    else
+    {
+        // HTTP Error
+        httpClient.end();
+    }
+    return false;
+}
+
+int PCEvent::numberOfEventsInDayOfThisMonth(int day)
+{
+    return PCEvent::_eventsInThisMonth.count(day);
+}
+
+std::vector<PCEvent> PCEvent::eventsInDayOfThisMonth(int day)
+{
+    std::vector<PCEvent> eventsInDay;
+    if (PCEvent::_eventsInThisMonth.count(day) > 0)
+    {
+        auto itr = PCEvent::_eventsInThisMonth.lower_bound(day);
+        auto last = PCEvent::_eventsInThisMonth.upper_bound(day);
+        while (itr != last)
+        {
+            eventsInDay.push_back(itr->second);
+            ++itr;
+        }
+    }
+    return eventsInDay;
+}
+
+int PCEvent::numberOfHolidaysInDayOfThisMonth(int day)
+{
+    return PCEvent::_holidaysInThisMonth.count(day);
+}
+
+std::vector<PCEvent> PCEvent::holidaysInDayOfThisMonth(int day)
+{
+    std::vector<PCEvent> eventsInDay;
+    if (PCEvent::_holidaysInThisMonth.count(day) > 0)
+    {
+        auto itr = PCEvent::_holidaysInThisMonth.lower_bound(day);
+        auto last = PCEvent::_holidaysInThisMonth.upper_bound(day);
+        while (itr != last)
+        {
+            eventsInDay.push_back(itr->second);
+            ++itr;
+        }
+    }
+    return eventsInDay;
+}
+std::vector<PCEvent> PCEvent::eventsInNextMonth()
+{
+    return PCEvent::_eventsInNextMonth;
+}
+
+// Other functions
+bool operator<(const PCEvent &left, const PCEvent &right)
+{
+    return (left.getTimeT() < right.getTimeT());
+}
+bool operator>(const PCEvent &left, const PCEvent &right)
+{
+    return (left.getTimeT() > right.getTimeT());
+}
+
+int dayOfWeek(int year, int month, int day)
+{
+    if (month < 3)
+    {
+        year--;
+        month += 12;
+    }
+    return (year + year / 4 - year / 100 + year / 400 + (13 * month + 8) / 5 + day) % 7;
+}
+
+int numberOfDaysInMonth(int year, int month)
+{
+    int numberOfDaysInMonthArray[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    {
+        numberOfDaysInMonthArray[1] = 29;
+    }
+    return numberOfDaysInMonthArray[month - 1];
+}
+
+tm tmFromICalDateString(String iCalDateString, float toTimezone)
+{
+    // 20240122T051119Z
+    // YYYYMMDD T hhmmss Z
+    tm timeInfo = {.tm_sec = 0, .tm_min = 0, .tm_hour = 0, .tm_mday = 0, .tm_mon = 0, .tm_year = 0};
+    if (iCalDateString.length() < 8)
+        return timeInfo;
+    timeInfo.tm_year = (iCalDateString.substring(0, 4)).toInt() - 1900;
+    timeInfo.tm_mon = (iCalDateString.substring(4, 6)).toInt() - 1;
+    timeInfo.tm_mday = (iCalDateString.substring(6, 8)).toInt();
+    timeInfo.tm_wday = dayOfWeek(timeInfo.tm_year + 1900, timeInfo.tm_mon + 1, timeInfo.tm_mday);
+
+    if (iCalDateString.length() < 15)
+        return timeInfo;
+    if (iCalDateString.charAt(8) != 'T')
+        return timeInfo;
+
+    timeInfo.tm_hour = (iCalDateString.substring(9, 11)).toInt();
+    timeInfo.tm_min = (iCalDateString.substring(11, 13)).toInt();
+    timeInfo.tm_sec = (iCalDateString.substring(13, 15)).toInt();
+
+    // Convert timezone
+    if (toTimezone != 0.0f)
+    {
+        int numberOfDaysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if ((timeInfo.tm_year + 1900) % 4 == 0 && (timeInfo.tm_year + 1900) % 100 != 0 || (timeInfo.tm_year + 1900) % 400 == 0)
+        {
+            numberOfDaysInMonth[1] = 29;
+        }
+
+        int secondsInDay = timeInfo.tm_hour * 3600 + timeInfo.tm_min * 60 + timeInfo.tm_sec;
+        int convertedSecondsInDay = secondsInDay + toTimezone * 3600;
+
+        if (convertedSecondsInDay < 0)
+        { // previous day
+            if (timeInfo.tm_mday == 1)
+            {
+                if (timeInfo.tm_mon == 0)
+                { // January
+                    if (timeInfo.tm_year > 1)
+                    {
+                        timeInfo.tm_year--;
+                    }
+                    timeInfo.tm_mon = 11;
+                }
+                else
+                {
+                    timeInfo.tm_mon--;
+                }
+                timeInfo.tm_mday = numberOfDaysInMonth[timeInfo.tm_mon];
+            }
+            else
+            {
+                timeInfo.tm_mday--;
+            }
+            convertedSecondsInDay += 24 * 3600;
+        }
+        else if (convertedSecondsInDay >= 24 * 3600)
+        { // next day
+            if (timeInfo.tm_mday == numberOfDaysInMonth[timeInfo.tm_mon])
+            {
+                if (timeInfo.tm_mon == 11)
+                {
+                    timeInfo.tm_year++;
+                    timeInfo.tm_mon = 0;
+                }
+                else
+                {
+                    timeInfo.tm_mon++;
+                }
+                timeInfo.tm_mday = 1;
+            }
+            else
+            {
+                timeInfo.tm_mday++;
+            }
+            convertedSecondsInDay -= 24 * 3600;
+        }
+        timeInfo.tm_hour = convertedSecondsInDay / 3600;
+        timeInfo.tm_min = (convertedSecondsInDay % 3600) / 60;
+        timeInfo.tm_sec = (convertedSecondsInDay % 3600) % 60;
+        timeInfo.tm_wday = dayOfWeek(timeInfo.tm_year + 1900, timeInfo.tm_mon + 1, timeInfo.tm_mday);
+    }
+
+    return timeInfo;
+}
---
+++ src/main.cpp
@@ -0,0 +1,406 @@
+#include <Arduino.h>
+
+#include <SPI.h>
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "epd13in3b.h"
+
+#include <vector>
+#include <map>
+#include <time.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClient.h>
+#include <Preferences.h>
+#include "SD_MMC.h"
+
+#include <LovyanGFX.hpp>
+
+#include "PCEvent.h"
+
+#define SD_MMC_CMD 38
+#define SD_MMC_CLK 39
+#define SD_MMC_D0 40
+
+#define LED_BUILTIN 2
+#define PIN_BUTTON 4
+
+#define VOLTAGE_TEST 8
+#define VOLTAGE_READ 9
+#define VOLTAGE_ADC_CHANNEL ADC_CHANNEL_8
+
+#define HEADER_HEIGHT 10
+#define FOOTER_HEIGHT 20
+#define COLUMN_WIDTH 137
+#define DAY_HEIGHT 42
+
+#define WHITE 255
+#define BLACK 0
+
+#define LARGE_FONT FreeSansBold24pt7b
+#define SMALL_FONT efontJA_14
+#define SMALL_FONT_HEIGHT 16
+
+#define uS_TO_S_FACTOR 1000000ULL
+
+#define holidayCacheKey "Holiday"
+
+#define LOG_ENABLE true
+#define LOG_VOLTAGE false
+#define LOG_HOLIDAY true
+#define LOG_HEAP true
+
+String pemFileName = "/root_ca.pem";
+std::vector<String> iCalendarURLs;
+String iCalendarHolidayURL;
+String rootCA = "";
+boolean loaded = false;
+boolean loginScreen = false;
+float timezone = 0;
+
+int currentYear = 0;
+int currentMonth = 0;
+int nextMonthYear = 0;
+int nextMonth = 0;
+String dateString = "";
+
+Preferences pref;
+String holidayCacheString;
+
+std::multimap<int, PCEvent> eventsInThisMonth;
+std::multimap<int, PCEvent> holidaysInThisMonth;
+std::vector<PCEvent> eventsInNextMonth;
+std::vector<PCEvent> eventsToDisplay;
+
+LGFX_Sprite blackSprite;
+LGFX_Sprite redSprite;
+
+void showCalendar();
+void loadICalendar(String urlString, boolean holiday);
+uint32_t readVoltage();
+void logLine(String line);
+void shutdown(int wakeUpSeconds);
+
+void setup()
+{
+  // put your setup code here, to run once:
+  blackSprite.setColorDepth(1);
+  blackSprite.createSprite(EPD_WIDTH, EPD_HEIGHT);
+  blackSprite.setTextWrap(false);
+
+  redSprite.setColorDepth(1);
+  redSprite.createSprite(EPD_WIDTH, EPD_HEIGHT);
+  redSprite.setTextWrap(false);
+
+  // SD Card
+  SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
+  if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5))
+  {
+    log_printf("Card Mount Failed\n");
+    return;
+  }
+  uint8_t cardType = SD_MMC.cardType();
+  if (cardType == CARD_NONE)
+  {
+    log_printf("No SD_MMC card attached\n");
+    return;
+  }
+
+  // Load settings from "settings.txt" in SD card
+  String wifiIDString = "wifiID";
+  String wifiPWString = "wifiPW";
+
+  File settingFile = SD_MMC.open("/settings.txt");
+  if (settingFile)
+  {
+    while (settingFile.available() > 0)
+    {
+      String line = settingFile.readStringUntil('\n');
+      if (line.startsWith("//"))
+        continue;
+      int separatorLocation = line.indexOf(":");
+      if (separatorLocation > -1)
+      {
+        String key = line.substring(0, separatorLocation);
+        String content = line.substring(separatorLocation + 1);
+
+        // WiFi SSID and paassword
+        if (key == "SSID")
+          wifiIDString = content;
+
+        else if (key == "PASS")
+          wifiPWString = content;
+
+        // HTTPS access
+        else if (key == "pemFileName")
+          pemFileName = content;
+
+        else if (key == "iCalendarURL")
+          iCalendarURLs.push_back(content);
+
+        else if (key == "holidayURL")
+          iCalendarHolidayURL = content;
+
+        else if (key == "timezone")
+          PCEvent::defaultTimezone = content.toFloat();
+      }
+    }
+    settingFile.close();
+
+    // Start Wifi connection
+    WiFi.begin(wifiIDString.c_str(), wifiPWString.c_str());
+    // Wait until wifi connected
+    int i = 0;
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(500);
+      log_printf(".");
+      i++;
+      if (i > 120)
+        break;
+    }
+    log_printf("\n");
+
+    // Setup NTP
+    configTime(60 * 60 * PCEvent::defaultTimezone, 0, "ntp.nict.jp", "ntp.jst.mfeed.ad.jp");
+
+    // Load PEM file in SD card
+    File pemFile = SD_MMC.open(pemFileName.c_str());
+    if (pemFile)
+    {
+      PCEvent::setRootCA(pemFile.readString());
+      pemFile.close();
+      log_printf("pem file loaded:%s\n", pemFileName.c_str());
+    }
+
+    // Load Holidays cache for this month
+    pref.begin("PaperCal", false);
+    holidayCacheString = pref.getString(holidayCacheKey, "");
+    Serial.println("Holiday loaded:\n" + holidayCacheString);
+    pref.end();
+
+    // Prepare voltage
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(VOLTAGE_TEST, OUTPUT);
+    pinMode(VOLTAGE_READ, ANALOG);
+  }
+  // light sleep wake up source
+  esp_sleep_enable_gpio_wakeup();
+  gpio_wakeup_enable(GPIO_NUM_17, GPIO_INTR_LOW_LEVEL); // BUSY_PIN
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  if (!loaded)
+  {
+    showCalendar();
+    loaded = true;
+    delay(1000);
+  }
+}
+
+void showCalendar()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  // Get local time
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    log_printf("Waiting getLocalTime\n");
+    delay(500);
+    return;
+  }
+
+  PCEvent::setTimeInfo(timeinfo);
+  PCEvent::setHolidayCacheString(holidayCacheString);
+
+  int year = PCEvent::currentYear;
+  int month = PCEvent::currentMonth;
+  int day = PCEvent::currentDay;
+
+  // Load iCalendar
+  for (auto &urlString : iCalendarURLs)
+  {
+    PCEvent::loadICalendar(urlString, false);
+  }
+  // Load iCalendar for holidays
+  if (!PCEvent::isCacheValid() && !iCalendarHolidayURL.isEmpty())
+  {
+    PCEvent::loadICalendar(iCalendarHolidayURL, true);
+    pref.begin("PaperCal", false);
+    pref.putString(holidayCacheKey, PCEvent::holidayCacheString());
+    Serial.println("Holiday cached:\n" + PCEvent::holidayCacheString());
+    pref.end();
+  }
+
+  WiFi.disconnect(true);
+
+  // Draw calendar
+  int firstDayOfWeek = dayOfWeek(year, month, 1);
+  int numberOfDays = numberOfDaysInMonth(year, month);
+  int numberOfRows = (firstDayOfWeek + numberOfDays - 1) / 7 + 1;
+  int rowHeight = (EPD_HEIGHT - (HEADER_HEIGHT + FOOTER_HEIGHT)) / numberOfRows;
+
+  blackSprite.fillScreen(WHITE);
+  redSprite.fillScreen(WHITE);
+
+  // draw horizontal lines
+  for (int i = 1; i <= numberOfRows; i++)
+  {
+    blackSprite.drawFastHLine(0, i * rowHeight + HEADER_HEIGHT, EPD_WIDTH, BLACK);
+  }
+
+  // draw vertical lines
+  int lineHeight = numberOfRows * rowHeight;
+  for (int i = 1; i < 7; i++)
+  {
+    blackSprite.drawFastVLine(i * COLUMN_WIDTH, HEADER_HEIGHT, lineHeight, BLACK);
+  }
+
+  // draw days in month
+  LGFX_Sprite *selectedSprite = &blackSprite;
+
+  for (int i = 1; i <= numberOfDays; i++)
+  {
+    int row = (firstDayOfWeek + i - 1) / 7;
+    int column = (6 + firstDayOfWeek + i) % 7;
+    boolean holiday = (column == 0 || column == 6 || (PCEvent::numberOfHolidaysInDayOfThisMonth(i) > 0)) ? true : false;
+    selectedSprite = holiday ? &redSprite : &blackSprite;
+
+    // invert color if it is today
+    uint16_t dayColor = BLACK;
+    if (day == i)
+    {
+      selectedSprite->fillRect(column * COLUMN_WIDTH, row * rowHeight + HEADER_HEIGHT, COLUMN_WIDTH, DAY_HEIGHT, BLACK);
+      dayColor = WHITE;
+    }
+
+    // draw day
+    selectedSprite->setFont(&fonts::LARGE_FONT);
+    int dayWidth = selectedSprite->textWidth(String(i));
+    selectedSprite->setTextColor(dayColor);
+    selectedSprite->setCursor(column * COLUMN_WIDTH + (COLUMN_WIDTH - dayWidth) / 2, row * rowHeight + 4 + HEADER_HEIGHT);
+    selectedSprite->printf("%d", i);
+
+    // draw events
+    std::vector<PCEvent> eventsInToday;
+    auto holidaysInDay = PCEvent::holidaysInDayOfThisMonth(i);
+    eventsInToday.insert(eventsToDisplay.begin(), holidaysInDay.begin(), holidaysInDay.end());
+    auto eventsInDay = PCEvent::eventsInDayOfThisMonth(i);
+    eventsInToday.insert(eventsToDisplay.begin(), eventsInDay.begin(), eventsInDay.end());
+
+    blackSprite.setFont(&fonts::SMALL_FONT);
+    redSprite.setFont(&fonts::SMALL_FONT);
+    blackSprite.setTextColor(BLACK);
+    redSprite.setTextColor(BLACK);
+    blackSprite.setClipRect(column * COLUMN_WIDTH, row * rowHeight + DAY_HEIGHT + HEADER_HEIGHT, COLUMN_WIDTH, rowHeight - DAY_HEIGHT);
+    redSprite.setClipRect(column * COLUMN_WIDTH, row * rowHeight + DAY_HEIGHT + HEADER_HEIGHT, COLUMN_WIDTH, rowHeight - DAY_HEIGHT);
+    if (eventsInToday.size() > 0)
+    {
+      int i = 0;
+      for (auto &event : eventsInToday)
+      {
+        selectedSprite = event.isHolidayEvent ? &redSprite : &blackSprite;
+        selectedSprite->setCursor(column * COLUMN_WIDTH + 2, row * rowHeight + DAY_HEIGHT + HEADER_HEIGHT + 4 + SMALL_FONT_HEIGHT * i);
+        selectedSprite->print("・" + event.getTitle());
+        i++;
+        if (i >= 3)
+          break;
+      }
+    }
+    blackSprite.clearClipRect();
+    redSprite.clearClipRect();
+  }
+
+  // Log
+  char logBuffer[32];
+  sprintf(logBuffer, "%d/%d/%d %02d:%02d:%02d", year, month, day, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+  String logString = String(logBuffer);
+
+  if (LOG_VOLTAGE)
+  {
+    uint32_t voltage = readVoltage();
+    logString += ", mV:";
+    logString += String(voltage);
+  }
+
+  if (LOG_HOLIDAY)
+  {
+    if (PCEvent::isCacheValid())
+    {
+      logString += ", Holiday cache:valid";
+    }
+    else
+    {
+      logString += ", Holiday cache:invalid";
+    }
+  }
+
+  if (LOG_HEAP)
+  {
+    logString += ", Heap:";
+    logString += String(esp_get_free_heap_size());
+  }
+
+  if (LOG_ENABLE)
+    logLine(logString);
+
+  // Footer
+  blackSprite.setFont(&fonts::SMALL_FONT);
+  blackSprite.setCursor(8, EPD_HEIGHT - FOOTER_HEIGHT);
+  blackSprite.print(logString);
+
+  // Display in EPD
+  Epd epd;
+  int initResult = epd.Init();
+  if (initResult != 0)
+  {
+    log_printf("e-Paper init failed: %d", initResult);
+    return;
+  }
+  epd.Displaypart((unsigned char *)(blackSprite.getBuffer()), 0, 0, EPD_WIDTH, EPD_HEIGHT, 0);
+  epd.Displaypart((unsigned char *)(redSprite.getBuffer()), 0, 0, EPD_WIDTH, EPD_HEIGHT, 1);
+  epd.Sleep();
+  digitalWrite(PWR_PIN, LOW);
+
+  // Deep sleep (wake up at 0:05)
+  loaded = true;
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  shutdown(24 * 3600 - (timeinfo.tm_hour * 3600 + timeinfo.tm_min * 60 + timeinfo.tm_sec) + 300);
+}
+
+uint32_t readVoltage()
+{
+  uint32_t voltage = 0;
+  esp_adc_cal_characteristics_t characteristics;
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_12Bit, 1100, &characteristics);
+  digitalWrite(VOLTAGE_TEST, HIGH);
+  if (esp_adc_cal_get_voltage(ADC_CHANNEL_8, &characteristics, &voltage) == ESP_OK)
+  {
+
+    digitalWrite(VOLTAGE_TEST, LOW);
+    return voltage;
+  }
+  digitalWrite(VOLTAGE_TEST, LOW);
+  return 0;
+}
+
+void logLine(String line)
+{
+  File logFile = SD_MMC.open("/log.txt", FILE_APPEND, true);
+  if (logFile)
+  {
+    logFile.println(line);
+  }
+  logFile.close();
+}
+
+void shutdown(int wakeUpSeconds)
+{
+  gpio_wakeup_disable(GPIO_NUM_17);
+
+  esp_sleep_enable_timer_wakeup(wakeUpSeconds * uS_TO_S_FACTOR);
+  esp_deep_sleep_start();
+}
